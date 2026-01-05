86dd2807be3347d165e8639f80a942865abeb4dc
sharkbot-neko <modoggu12345@outlook.com>
2026-01-06 07:23:19 +0900

    lookupを権限者のみに限定

--- src/commands/lookup.ts
+++ src/commands/lookup.ts
@@ -1,9 +1,13 @@
+import role from "../lib/role.ts";
+
 export default {
     name: "lookup",
     execute: async (client: any, name: string, args: string[], message: any, op: any) => {
         if (args.length < 1) {
             return;
         }
+
+        if (await role.getPerm(message.to, message.from) < 5) return;
 
         const midData = await client.talk.getContact({ mid: args[0] });
 
