693bf1892439cd3031289359f847c009670058d6
sharkbot-neko <modoggu12345@outlook.com>
2026-01-06 12:58:32 +0900

    しっかりと計測できるように

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,10 +1,17 @@
 export default {
     name: "ping",
     execute: async (client: any, name: string, args: string[], message: any, op: any) => {
+		const start = Date.now();
 		await client.talk.sendMessage({
 			to: message.to === client.profile?.mid ? message.from : message.to,
-			text: "pong!",
+			text: "計測中・・",
 			e2ee: !!op.message.chunks,
 		});
-    }
+        const end = Date.now();
+		await client.talk.sendMessage({
+			to: message.to === client.profile?.mid ? message.from : message.to,
+			text: `🏓Pong! ${end - start}m`,
+			e2ee: !!op.message.chunks,
+		});
+	}
 }
